d13edca48fece429cc954e3b8f3dbdbb8cbb5b9f
DeadNews <uhjnnn@gmail.com>
2022-07-24 15:33:20 +0200

    Init

---
+++ src/deadnews_python_template/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Docstring
+"""
+from __future__ import annotations
+
+
+def get_answer() -> int:
+    """Get answer to the Meaning of Life, the Universe, and Everything."""
+    return 40 + 2
---
+++ tests/test_deadnews_python_template.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+from src.deadnews_python_template import get_answer
+
+def test_get_answer():
+    assert get_answer() == 42
